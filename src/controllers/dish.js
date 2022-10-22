61743d6a9ced1c5e3920bdd14bba4f92bf9d6688
nowkung <nowjanpi1540@gmail.com>
2022-10-22 14:30:33 +0700

    [ADD] Portion

--- src/controllers/dish.js
+++ src/controllers/dish.js
@@ -6,7 +6,7 @@ const knex = require("../services/db");
 async function getDish(req, res, next){
     try{
         // get data
-        const dish = await knex('Food Nutrition Fact').select('FoodID', 'FoodNameENG', 'FoodNameTH', 'Calories', 'Fat', 'Carb', 'Protein', 'Sodium' )
+        const dish = await knex('Food Nutrition Fact').select('FoodID', 'FoodNameENG', 'FoodNameTH', 'Calories', 'Fat', 'Carb', 'Protein', 'Sodium', 'Portion', )
         
         return res.status(200).json({ status: 'SUCCESS', dish })
     } catch(err) {
