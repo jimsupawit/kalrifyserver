37d067fb57796767935b253903bebb6b318168df
nowkung <nowjanpi1540@gmail.com>
2022-10-24 01:13:01 +0700

    minor fix

--- src/controllers/analyze.js
+++ src/controllers/analyze.js
@@ -5,6 +5,7 @@ const knex = require("../services/db");
 
 async function getAnalyze(req, res, next){
     try{
+        console.log(req.headers)
         // get data
         const analyze = await knex('Food Nutrition Fact').where("FoodNameENG", req.body).select('FoodID', 'FoodNameENG', 'FoodNameTH', 'Calories', 'Fat', 'Carb', 'Protein', 'Sodium', 'Portion', )
         
