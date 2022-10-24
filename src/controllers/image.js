f378a58ba4fcfd66079e7f7360440629ae33692b
nowkung <nowjanpi1540@gmail.com>
2022-10-24 22:06:28 +0700

    minor fix

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -6,7 +6,7 @@ const knex = require("../services/db");
 async function getImage(req, res, next){
     try{
         // get data
-        const image = await knex('Food Nutrition Fact').where("FoodNameENG", req.headers["food"]).select('Image',)
+        const image = await knex('Food Nutrition Fact').where("FoodNameENG", req.headers["food"]).select()
         
         return res.status(200).json({ status: 'SUCCESS', image })
     } catch(err) {
