b2d8ee38cfc42e7c5d914712477658e31218f86d
nowkung <nowjanpi1540@gmail.com>
2022-10-24 18:25:23 +0700

    [UPDATE] Dish Route

--- src/controllers/dish.js
+++ src/controllers/dish.js
@@ -6,7 +6,7 @@ const knex = require("../services/db");
 async function getDish(req, res, next){
     try{
         // get data
-        const dish = await knex('Food Nutrition Fact').select('FoodID', 'FoodNameENG', 'FoodNameTH', 'Calories', 'Fat', 'Carb', 'Protein', 'Sodium', 'Portion', )
+        const dish = await knex('Food Nutrition Fact').select('FoodID', 'FoodNameENG', 'FoodNameTH', 'Calories', 'Fat', 'Carb', 'Protein', 'Sodium', 'Portion', 'Image')
         
         return res.status(200).json({ status: 'SUCCESS', dish })
     } catch(err) {
