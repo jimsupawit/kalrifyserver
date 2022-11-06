d58f313ca3dc2ff12e2c2dcfb4aa0f5e2a4b21ea
nowkung <nowjanpi1540@gmail.com>
2022-11-06 21:32:26 +0700

    test

--- src/controllers/diary.js
+++ src/controllers/diary.js
@@ -18,23 +18,23 @@ async function getDiary(req, res, next){
 async function addDiary(req, res, next) {
     // const { id } = req.user;
     const { id } = 1;
-    const date = req.body["date"];
-    const total = req.body["total"];
-    const dish = req.body["dish"];
+    // const date = req.body["date"];
+    // const total = req.body["total"];
+    // const dish = req.body["dish"];
     // foodNameENG, foodNameTH, Calories, Fat, Carb, Protein, Sodium, Portion
     // {"foodNameENG": foodNameENG, "foodNameTH":foodNameTH, "Calories":Calories, "Fat":Fat, "Carb":Carb, "Protein":Protein, "Sodium":Sodium, "Portion":Portion}
 
     // const check = await knex('UserDiary').select(coalesce(sum(date),0)).where({ uid:id, date:date })
     // print(check);
-    const dishList = {body: [dish]}
+    const dishList = {body: [req.body.dish]}
     try {
         // if(check=0){
     
-                const diary = await knex('UserDiary').insert({ uid:id, date:date, totalCal:total, dishList:dishList })
+                const diary = await knex('UserDiary').insert({ uid:id, date:req.body.date, totalCal:req.body.total, dishList:dishList })
     
                 // const token = jwt.sign({ id: id[0] }, process.env.TOKEN_KEY);
     
-                return res.status(200).json({ status: 'SUCCESS'})
+                return res.status(200).json({ status: 'SUCCESS', diary})
         // }else{
         //         const oldTotal = await knex('UserDiary').select(total).where({ uid:id, date:date })
         //         const newList = await knex('UserDiary').select(dishList).where({ uid:id, date:date })
